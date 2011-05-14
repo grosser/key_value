2497775d1b88a790fd94dcdd976ce66ca6b4bc00
grosser <grosser.michael@gmail.com>
2011-05-14 17:09:14 +0200

    make specs runnable in mysql

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'activerecord'
 
 group :dev do # not development <-> would add unneeded development dependencies in gemspec
   gem 'sqlite3'
+  gem 'mysql2', '~>0.2.0'
   gem 'rake'
   gem 'rspec', '~>2'
   gem 'jeweler'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,24 @@
 require 'active_record'
 
-# connect                                                                                                                                                                                                                                     
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => ":memory:"
-)
+# connect
+if ENV['DB'] == 'mysql'
+  puts 'using mysql'
+  ActiveRecord::Base.establish_connection(
+    :adapter => "mysql2",
+    :database => "key_values_test"
+  )
+else
+  puts 'using sqlite'
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => ":memory:"
+  )
+end
 
 # create tables
 ActiveRecord::Schema.define(:version => 1) do
+  drop_table :key_values rescue nil
+
   create_table :key_values do |t|
     t.string :key, :null => false
     t.text :value, :null => false
